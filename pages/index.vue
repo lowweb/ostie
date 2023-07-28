dce88f5495492b402aee6449bb98a74a5dfe96d6
lowweb <looper@MacBook-Pro-looper.local>
2023-07-28 21:55:27 +1000

    add anime on start

--- pages/index.vue
+++ pages/index.vue
@@ -22,6 +22,7 @@ import ResultMovie from '~/components/resultMovie.vue'
 import Title from '~/components/title.vue'
 import Footer from '~/components/footer.vue'
 import LangSwitch from '~/components/langSwitch.vue'
+import anime from '../node_modules/animejs/lib/anime.es.js';
 export default {
     async fetch ({store}) {
         await store.dispatch("recent/getRecentData")
@@ -39,6 +40,76 @@ export default {
     Footer,
     LangSwitch
     // smartMusic
+  },
+
+  mounted() {
+    
+    const containerWidth = document.querySelector('.container').offsetWidth;
+    const svgWidth = document.querySelector('.header__start a').offsetWidth;
+
+    // console.log(containerWidth)
+    // console.log(svgWidth)
+    let positionCenter = containerWidth/2 - svgWidth/2;
+    if (containerWidth < 792) {
+      positionCenter = positionCenter - 24;
+    }
+    
+    anime.timeline({loop: false})
+        .add({
+          targets: '.header__start .header__logo',
+          translateX: positionCenter
+        })
+        .add({
+          targets: '.header__start .header__logo',
+          opacity: [0, 1],
+          easing: 'linear',
+          duration: 10
+        })
+        .add({
+            targets: '.header__start .logo__lines path',
+            strokeDashoffset: [anime.setDashoffset, 0],
+            easing: 'easeInOutSine',
+            duration: 1300,
+            delay: function(el, i) { return i * 250 },
+            direction: 'alternate',
+        },'-=300')
+        .add({
+            targets: '.header__start .header__logo',
+            translateX: [positionCenter, 0],
+            left: 'auto',
+            easing: 'spring(1, 80, 10, 0)',
+            duration: 300,
+        },'-=900')
+        .add({
+          targets: '.header .logo__fill',
+          opacity: [0, 1],
+          easing: 'linear',
+          duration: 500
+        },'-=1000')
+        .add({
+          targets: '.header__start',        
+          opacity: [1, 0],
+          duration: 550,
+          easing: 'easeInOutQuad',
+          complete: function() {
+            document.querySelector('.header__start').style.display = 'none';
+          }
+        },'-=800')
+        .add({
+          targets: '.recent__item',
+          opacity: [0, 1],
+          translateX: [100, 0],
+          duration: 650,
+          delay: anime.stagger(150)
+        },'-=2500')
+        .add({
+          targets: '.lang-switch__el',
+          opacity: [0, 1],
+          translateY: [-10, 0],
+          duration: 850,
+          delay: anime.stagger(200)
+        },'-=2500')
+        
   }
 }
 
@@ -66,6 +137,7 @@ main{
   display: flex;
   flex-direction: column;
   flex: 1 0 auto;
+  position: relative;
 }
 
 
